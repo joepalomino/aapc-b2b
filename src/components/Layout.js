9c8dd2932a95b40893b1f0d56c0252653700f15e
joseph palomino <joe.palomino50@gmail.com>
2020-08-13 09:42:26 -0500

    update file name

---
+++ src/components/Layout.js
@@ -0,0 +1,43 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+import Menu from "./Menu"
+import tw from 'tailwind.macro'
+
+/**@jsx jsx */
+import { jsx } from "@emotion/core"
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <Menu />
+      <main css={tw`pt-24`}>{children}</main>
+      <footer>
+
+      </footer>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
