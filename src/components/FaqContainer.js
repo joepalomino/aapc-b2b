586afbac4f26cdb47ff5a90a0f7e7f9f99699481
joseph palomino <joe.palomino50@gmail.com>
2020-09-11 16:38:30 -0500

    add faq schema

--- src/components/FaqContainer.js
+++ src/components/FaqContainer.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import tw from "tailwind.macro"
 import { Section, mq } from "./SharedStyledComponents"
+import Helmet from "react-helmet"
 
 /** @jsx jsx */
 import { jsx } from "@emotion/core"
@@ -25,9 +26,9 @@ const FAQHeader = styled.header(
       "transform .3s cubic-bezier(0.25, 0.1, 0.25, 1),box-shadow .3s cubic-bezier(0.25, 0.1, 0.25, 1)",
   },
   props => {
-    if(props.active) {
+    if (props.active) {
       return {
-        borderColor: 'transparent'
+        borderColor: "transparent",
       }
     }
   }
@@ -46,32 +47,34 @@ const FAQ = ({ question, answer: { answer } }) => {
   const [isExpanded, setIsExpanded] = useState(false)
   const handleFAQClick = e => setIsExpanded(!isExpanded)
   return (
-    <FAQWrapper active={isExpanded}>
-      <FAQHeader
-        role="button"
-        aria-expanded="false"
-        tabindex="0"
-        onClick={e => handleFAQClick(e)}
-        active={isExpanded}
-      >
-        <div css={tw`flex items-center`}>
-          <h3 css={tw`text-xl text-b2b-gray-dark font-bold`}>{question}</h3>
-          <div css={tw`ml-auto`}>
-            <svg
-              id="angle-down-solid"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 64 64"
-              width="16"
-            >
-              <path d="M56.293 16.003c1.761-1.645 4.553-1.645 6.314 0 1.857 1.734 1.857 4.623 0 6.357l-27.45 25.638c-1.761 1.645-4.553 1.645-6.314 0l-27.45-25.638c-0.899-0.839-1.393-1.963-1.393-3.178s0.494-2.339 1.393-3.178c1.761-1.645 4.553-1.645 6.314 0l24.293 22.689 24.293-22.689z"></path>
-            </svg>
+    <>
+      <FAQWrapper active={isExpanded}>
+        <FAQHeader
+          role="button"
+          aria-expanded="false"
+          tabindex="0"
+          onClick={e => handleFAQClick(e)}
+          active={isExpanded}
+        >
+          <div css={tw`flex items-center`}>
+            <h3 css={tw`text-xl text-b2b-gray-dark font-bold`}>{question}</h3>
+            <div css={tw`ml-auto`}>
+              <svg
+                id="angle-down-solid"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 64 64"
+                width="16"
+              >
+                <path d="M56.293 16.003c1.761-1.645 4.553-1.645 6.314 0 1.857 1.734 1.857 4.623 0 6.357l-27.45 25.638c-1.761 1.645-4.553 1.645-6.314 0l-27.45-25.638c-0.899-0.839-1.393-1.963-1.393-3.178s0.494-2.339 1.393-3.178c1.761-1.645 4.553-1.645 6.314 0l24.293 22.689 24.293-22.689z"></path>
+              </svg>
+            </div>
           </div>
-        </div>
-      </FAQHeader>
-      <FAQContent active={isExpanded}>
-        <p>{answer}</p>
-      </FAQContent>
-    </FAQWrapper>
+        </FAQHeader>
+        <FAQContent active={isExpanded}>
+          <p>{answer}</p>
+        </FAQContent>
+      </FAQWrapper>
+    </>
   )
 }
 
@@ -80,14 +83,33 @@ const FaqContainer = ({
     heading: { headline, subheadline },
     faqs,
   },
-}) => (
-  <Section headline={headline} subheadline={subheadline}>
-    <div css={tw`m-auto max-w-4xl`}>
-      {faqs.map(({ question, answer, id }) => (
-        <FAQ question={question} answer={answer} key={id} />
-      ))}
-    </div>
-  </Section>
-)
+}) => {
+  let faqSchema = {
+    "@context": "https://schema.org",
+    "@type": "FAQPage",
+    mainEntity: faqs.map(({ question, answer }) => ({
+      "@type": "Question",
+      "name": question,
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": answer.answer,
+      },
+    })),
+  }
+  return (
+    <>
+      <Helmet>
+        <script type="application/ld+json">{JSON.stringify(faqSchema)}</script>
+      </Helmet>
+      <Section headline={headline} subheadline={subheadline}>
+        <div css={tw`m-auto max-w-4xl`}>
+          {faqs.map(({ question, answer, id }) => (
+            <FAQ question={question} answer={answer} key={id} />
+          ))}
+        </div>
+      </Section>
+    </>
+  )
+}
 
 export default FaqContainer
